b6544bad2078b9fdc59dbdab5b3b08e467b3b5c7
AyaanORIGINAl <abc898528@gmail.com>
2025-06-05 12:48:06 +0600

    birthday

--- getting started with programming/birthday.py
+++ getting started with programming/birthday.py
@@ -1,8 +1,20 @@
-name = 'Yamal'
-day = 11
-month = 'July'
-year = 2007
-birthday_format = (day, month, year)
+friend1_name = "Yamal"
+friend1_birthday = "11 July 2012"
 
-print(f"Name: {name}")
-print(f"Birthday: {birthday_format}")
+friend2_name = "Zayan"
+friend2_birthday = "12 February 2012"
+
+friend3_name = "Muhammad"
+friend3_birthday = "23 March 2012"
+
+friend4_name = "John"
+friend4_birthday = "8 April 2012"
+
+friend5_name = "Rahul"
+friend5_birthday = "30 May 2013"
+
+print(friend1_name + "'s birthday is: " + friend1_birthday)
+print(friend2_name + "'s birthday is: " + friend2_birthday)
+print(friend3_name + "'s birthday is: " + friend3_birthday)
+print(friend4_name + "'s birthday is: " + friend4_birthday)
+print(friend5_name + "'s birthday is: " + friend5_birthday)
