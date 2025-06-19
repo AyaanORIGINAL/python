6883698011f6a27a9be7e3f4986fe972ff061bbe
AyaanORIGINAl <abc898528@gmail.com>
2025-06-19 11:55:25 +0600

    alphabet

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+character = input ("Enter a single character:" )
+
+if (character >= "a" and character <= "z") or (character >= "A" and character <= "Z"):
+    print("This is an alphabet letter.")
+else:
+    print("This is not an alphabet letter.")
