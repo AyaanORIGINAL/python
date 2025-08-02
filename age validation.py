c15adbba799a60f9eb25d9fa2b84881768d48448
AyaanORIGINAl <abc898528@gmail.com>
2025-08-02 11:43:58 +0600

    age validation

--- age validation.py
+++ age validation.py
@@ -1,2 +1,8 @@
 try:
     age = int(input("Enter your age using numbers: "))
+    print("Your age is", age)
+
+except ValueError:
+    print("Please enter valid number")
+except NameError as ex:
+    print("Name error is:", ex)
